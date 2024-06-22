7d066cf909b0c582206c98d8236ea99f4a56b600
hendrik <24483010+kwagga333@users.noreply.github.com>
2024-06-22 23:55:22 +0200

    Add getBoard and rows and col nad background
    event listener for rows for background color and getBoard function

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
 
 
+const display = document.getElementById('display');
+
+
+function getBoard(size){
+for(let i = 0; i < size; i++){
+
+    const col = document.createElement('div');
+    col.classList.add("col");
+    display.appendChild(col);
+    for(let j = 0; j < size; j++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        col.appendChild(row);
+        row.addEventListener('mouseover', () => {
+            row.style.backgroundColor = "red";
+        });           
+        };  
+       
+};
+
+};
+
+getBoard(25);
